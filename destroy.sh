8f71d223b667ba882c7eb9ff9434219e8f869ca2
Marco Cotrotzo <marcotrotzo@gmail.com>
2026-06-21 15:14:03 +0200

    test

---
+++ destroy.sh
@@ -0,0 +1,17 @@
+for twin in Battery PV; do
+    cp -r ./DigitalTwinProfileSysMLv2/output/$twin/. ./digital-twin-manager/
+    cp config_credentials.json ./digital-twin-manager/
+    cd digital-twin-manager/src
+    python main.py <<EOF
+destroy
+EOF
+    cd ..
+done
+
+
+cd CloudDeployerTestSimulator
+pulumi destroy --yes
+cd ..
+
+cd output
+terraform destroy -auto-approve
